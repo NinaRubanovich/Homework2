cf01e323e0c590f60b8a232f27f57f12ac477465
Nina Rubanovich <ninaruba01@gmail.com>
2023-09-19 16:28:49 -0500

    tidying up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import agent1
+# feel free to import your personal files here
 
 
 def uniform_cost_tree_search():
@@ -10,31 +11,47 @@ def uniform_cost_graph_search():
     pass
 
 
-def iterative_deepening_tree_search(initial_state, max_depth):
-    for depth in range(max_depth + 1):
-        result = depth_limited_search(initial_state, depth)
-        if result:
-            return result
+def iterative_deepening_tree_search(initial_state):
+    depth_limit = 0
+    while True:
+        result, cutoff = depth_limited_search(initial_state, depth_limit)
+        if result == "found":
+            return cutoff
+        elif result == "cutoff":
+            depth_limit += 1
+        elif result == "not_found":
+            return None
 
 
-def depth_limited_search(initial_state, depth):
+def depth_limited_search(state, depth_limit):
     # This is a helper function for iterative_deepening_tree_search
-    def recursive_dls(node, problem, limit):
-        if agent1.is_goal_state(node.state):
-            return agent1.extract_solution(node)
-        elif limit == 0:
-            return "cutoff"
-        else:
-            cutoff_occured = False
-            for child_node in agent1.expand(node):
-                result = recursive_dls(child_node, problem, limit - 1)
-                if result == "cutoff":
-                    cutoff_occured = True
-                elif result != "failure":
-                    return result
-                return "cutoff" if cutoff_occured else "failure"
+    return recursive_dls(agent1.Node(state, None, None, 0), depth_limit)
 
-    return recursive_dls(agent1.Node(initial_state, None, None, 0), None, depth)
+
+def recursive_dls(node, depth_limit):
+    if node.state.current_room_is_clean() and len(node.state.dirty_squares) == 0:
+        return "found", node
+
+    if depth_limit == 0:
+        return "cutoff", None
+
+    cutoff_occurred = False
+
+    for action in ["Left", "Right", "Up", "Down", "Suck"]:
+        if agent1.action_is_applicable(node.state, action):
+            child_state = agent1.apply_action(node.state, action)
+            if child_state:
+                child_node = agent1.Node(child_state, action, node, agent1.get_action_cost(action))
+                result, result_node = recursive_dls(child_node, depth_limit - 1)
+                if result == "found":
+                    return "found", result_node
+                elif result == "cutoff":
+                    cutoff_occurred = True
+
+    if cutoff_occurred:
+        return "cutoff", None
+    else:
+        return "not_found", None
 
 
 # Press the green button in the gutter to run the script.
@@ -42,4 +59,7 @@ if __name__ == '__main__':
     # initialize state
     agent_location = (2, 2)
     dirty_squares = [(1, 2), (2, 4), (3, 5)]
+
+    # initial_state = agent1.State(agent_location, dirty_squares)
+    # iterative_deepening_tree_search(initial_state)
 
