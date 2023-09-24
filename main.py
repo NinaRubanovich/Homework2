5f4d311fcbe0482ad96f712d6bcefaa37dbc832e
Gavin Boley <gzbkxc@umsystem.edu>
2023-09-24 17:41:12 -0500

    Comments and readability

--- main.py
+++ main.py
@@ -7,83 +7,86 @@ import time
 def uniform_cost_tree_search(problem):
     fringe = [((problem.initial_location,), 0, problem)]  # Initialize fringe with an empty path and dirty squares
     
+    # Initialize values
     nodes_expanded = 0
     nodes_generated = 1  
-    start_time = time.time()
+    start_time = time.time() # Always starts with first node expanded
 
     while fringe:
         fringe.sort(key=lambda x: (x[1], x[0]))  # Sort by cost, then coordinates for tie-breaking
-        path, cost, current_state = fringe.pop(0)
+        path, cost, current_state = fringe.pop(0) # Removes and returns the element at the front
 
-        if current_state.is_goal_state():
+        if current_state.is_goal_state(): # Checks if all rooms are clean and if so, ends
             execution_time = time.time() - start_time
             return path[1:], nodes_expanded, nodes_generated, execution_time
 
         nodes_expanded += 1
-        successors = current_state.get_successors()
+        successors = current_state.get_successors() # Determines next possible states
         for action, successor in successors:
             nodes_generated += 1
-            fringe.append((path + (action,), cost + ACTION_COSTS[action], successor))
+            fringe.append((path + (action,), cost + ACTION_COSTS[action], successor)) # Adds new element to fringe to be explored, represemts the overall successor state including new path cost
 
     return None
 
 def uniform_cost_graph_search(problem):
-    closed = set()  
+    closed = set() # Keeps track of explored states to avoid revision
     fringe = [((problem.initial_location,), 0, problem, problem.dirty_squares)]  # Initialize fringe with an empty path and dirty squares
     
+    # Initialize values
     nodes_expanded = 0
-    nodes_generated = 1 
+    nodes_generated = 1 # Always starts with first node expanded
     start_time = time.time()
 
-    while fringe:
+    while fringe: # While there is elements still in queue to be expanded
         fringe.sort(key=lambda x: (x[1], x[2].current_location, x[3]))  # Sort by cost, then location, then dirty squares for tie-breaking
-        path, cost, current_state, dirty_squares = fringe.pop(0)
+        path, cost, current_state, dirty_squares = fringe.pop(0) # Removes and returns the element at the front
 
-        if current_state.is_goal_state():
+        if current_state.is_goal_state(): # Checks if all rooms are clean and if so, ends
             execution_time = time.time() - start_time
             return path[1:], nodes_expanded, nodes_generated, execution_time
 
-        if current_state not in closed: 
-            closed.add(current_state)
+        if current_state not in closed: # Checks if statea hasnt been visited so far, if not, it will be expanded
+            closed.add(current_state) # Marks current state as visited
             nodes_expanded += 1
-            successors = current_state.get_successors()
+            successors = current_state.get_successors() # Determines next possible states
             for action, successor in successors:
                     nodes_generated += 1
-                    new_dirty_squares = successor.dirty_squares
-                    fringe.append((path + (action,), cost + ACTION_COSTS[action], successor, new_dirty_squares))
+                    new_dirty_squares = successor.dirty_squares # Update dirty squares for new state
+                    fringe.append((path + (action,), cost + ACTION_COSTS[action], successor, new_dirty_squares)) # Adds new element to fringe to be explored, represemts the overall successor state including the new path cost
 
-    return None
+    return None # Failure to find solution
 
 def iterative_deepening_tree_search(problem):
     max_depth = 0
-    while True:
-        result = depth_limited_tree_search(problem, max_depth)
+    while True: # Continues until a solution is found or every possible depth is reached
+        result = depth_limited_tree_search(problem, max_depth) # Explores nodes only up to a certain depth
         if result is not None:
             return result
-        max_depth += 1
+        max_depth += 1 # If no solution, go deeper
 
 
 def depth_limited_tree_search(problem, limit):
     fringe = [((problem.initial_location,), 0, problem)] # Initialize the closed set as an empty set
     
+    # Initialize values
     nodes_expanded = 0
     nodes_generated = 1 
     start_time = time.time()
 
-    while fringe:
+    while fringe: # While there is elements still in queue to be expanded
         fringe.sort(key=lambda x: (x[1], x[0]))  # Sort by cost, then coordinates for tie breaking
-        path, cost, current_state = fringe.pop(0)
+        path, cost, current_state = fringe.pop(0) # Removes and returns the element at the front
 
-        if current_state.is_goal_state():
+        if current_state.is_goal_state(): # Checks if all rooms are clean and if so, ends
             execution_time = time.time() - start_time
             return path[1:], nodes_expanded, nodes_generated, execution_time
 
-        if len(path) <= limit:
+        if len(path) <= limit: # Checks if the length of the path from the root node to the current node is within the specified limit
             nodes_expanded += 1
-            successors = current_state.get_successors()
+            successors = current_state.get_successors() # If so, continues to explore successors
             for action, successor in successors:
                 nodes_generated += 1
-                fringe.append((path + (action,), cost + ACTION_COSTS[action], successor))
+                fringe.append((path + (action,), cost + ACTION_COSTS[action], successor)) # Adds new element to fringe to be explored, represemts the overall successor state including the new path cost
 
     return None
 
